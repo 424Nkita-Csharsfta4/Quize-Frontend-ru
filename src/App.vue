c55eeaee93d695fa47af77011eba2085b18f1504
Nikita_Developer_PMR <103760832+424Nkita-Csharsfta4@users.noreply.github.com>
2022-08-13 17:28:06 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,270 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+const questions = ref([
+  {
+	question: 'Что такое ООП',
+	answer: 0,
+	options: [
+		'Обьектно ориентированое програмирование',
+		'Обькты что то там происходит',
+		'Это язык програмирования'
+	],
+	selected: null
+  },
+  {
+	question: 'CSS. Какое свойство CSS позволяет указать, что список ol должен быть пронумерован в обратном порядке?',
+	answer: 1,
+	options: [
+		'reverse',
+		'direction',
+		'list-direction',
+		'Такого CSS свойства несуществует'
+	],
+	selected: null
+  },
+  {
+	question: 'CSS. Выберите пример некорректного использования переменной в файлах стилей.?',
+	answer: 2,
+	options: [
+		'color:var(--my-var)',
+		'color:var(--my-var,red)',
+		'Все ответы правильны',
+		'Тут больше одного некорректно'
+	],
+	selected: null
+  }, {
+	question: 'HTML. Id какого элемента необходимо указать в атрибуте list тега input, чтобы при наборе текста в этом инпуте всплывал список вариантов (автокомплит)?',
+	answer: 2,
+	options: [
+		'input',
+		'list',
+		'options',
+		'datalist'
+	],
+	selected: null
+  },
+  {
+	question: 'JavaScript. Имеется элемент с определенным атрибутом. Если к этому элементу применить метод Element.setAttribute() с этим же атрибутом, то...',
+	answer: 2,
+	options: [
+		'Атрибут удалится',
+		'Значение атрибута изменится в соответствии с аргументами метадома',
+		'Возникнит ошибка',
+		'Нечего не произойдет'
+	],
+	selected: null
+  }
+])
+
+const quizCompleted = ref(false)
+const currentQuestion = ref(0)
+const score = computed(() => {
+	let value = 0
+	questions.value.map(q => {
+		if (q.selected != null && q.answer == q.selected) {
+			console.log('correct');
+			value++
+		}
+	})
+	return value
+})
+
+const getCurrentQuestion = computed(() => {
+	let question = questions.value[currentQuestion.value]
+	question.index = currentQuestion.value
+	return question
+})
+
+const SetAnswer = (e) => {
+	questions.value[currentQuestion.value].selected = e.target.value
+	e.target.value = null
+}
+
+const NextQuestion = () => {
+	if (currentQuestion.value < questions.value.length - 1) {
+		currentQuestion.value++
+		return
+	}
+	
+	quizCompleted.value = true
+}
+</script>
+
+<template>
+	<main class="app">
+		<h1>Насколько хорошо ты знаешь Frontend</h1>
+		<img  src="https://c.tenor.com/_HwQHDixHnMAAAAM/kitten-cat.gif" alt="">
+		
+		<section class="quiz" v-if="!quizCompleted">
+			<div class="quiz-info">
+				<span class="question">{{ getCurrentQuestion.question }}</span>
+				<span class="score">Правильно {{ score }}/{{ questions.length }}</span>
+			</div>
+			
+			<div class="options">
+				<label 
+					v-for="(option, index) in getCurrentQuestion.options" 
+					:for="'option' + index" 
+					:class="`option ${
+						getCurrentQuestion.selected == index 
+							? index == getCurrentQuestion.answer 
+								? 'correct' 
+								: 'wrong'
+							: ''
+					} ${
+						getCurrentQuestion.selected != null &&
+						index != getCurrentQuestion.selected
+							? 'disabled'
+							: ''
+					}`">
+					<input 
+						type="radio" 
+						:id="'option' + index" 
+						:name="getCurrentQuestion.index" 
+						:value="index" 
+						v-model="getCurrentQuestion.selected" 
+						:disabled="getCurrentQuestion.selected"
+						@change="SetAnswer" 
+					/>
+					<span>{{ option }}</span>
+				</label>
+			</div>
+			
+			<button 
+				@click="NextQuestion" 
+				:disabled="!getCurrentQuestion.selected">
+				{{ 
+					getCurrentQuestion.index == questions.length - 1 
+						? 'Finish' 
+						: getCurrentQuestion.selected == null
+							? 'Выберите вариант'
+							: 'Следующий вопрос'
+				}}
+			</button>
+		</section>
+
+		<section v-else>
+			<h2>Не плохой результат</h2>
+			<p>Правильно ответов {{ score }}/{{ questions.length }}
+			 <img src="https://i0.wp.com/i.giphy.com/media/zOvBKUUEERdNm/giphy-downsized.gif?w=770&ssl=1" alt=""></p>
+		</section>
+	</main>
+</template>
+
+<style>
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+	font-family: 'Montserrat', sans-serif;
+}
+
+body {
+	background-color: #300404;
+	color: #FFF;
+}
+
+.app {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding: 2rem;
+	height: 100vh;
+}
+
+h1 {
+	font-size: 2rem;
+	margin-bottom: 2rem;
+}
+
+.quiz {
+	background-color: #3f4f35;
+	padding: 1rem;
+	width: 100%;
+	max-width: 640px;
+}
+
+.quiz-info {
+	display: flex;
+	justify-content: space-between;
+	margin-bottom: 1rem;
+}
+
+.quiz-info .question {
+	color: #0c0c0c;
+	font-size: 1.25rem;
+}
+
+.quiz-info.score {
+	color: #FFF;
+	font-size: 1.25rem;
+}
+
+.options {
+	margin-bottom: 1rem;
+}
+
+.option {
+	padding: 1rem;
+	display: block;
+	background-color: #271c36;
+	margin-bottom: 0.5rem;
+	border-radius: 0.5rem;
+	cursor: pointer;
+}
+
+.option:hover {
+	background-color: #2d213f;
+}
+
+.option.correct {
+	background-color: #2cce7d;
+}
+
+.option.wrong {
+	background-color: #ff5a5f;
+}
+
+.option:last-of-type {
+	margin-bottom: 0;
+}
+
+.option.disabled {
+	opacity: 0.5;
+}
+
+.option input {
+	display: none;
+}
+
+button {
+	appearance: none;
+	outline: none;
+	border: none;
+	cursor: pointer;
+	padding: 0.5rem 1rem;
+	background-color: #2cce7d;
+	color: #2d213f;
+	font-weight: 700;
+	text-transform: uppercase;
+	font-size: 1.2rem;
+	border-radius: 0.5rem;
+}
+
+button:disabled {
+	opacity: 0.5;
+}
+
+h2 {
+	font-size: 2rem;
+	margin-bottom: 2rem;
+	text-align: center;
+}
+
+p {
+	color: #8F8F8F;
+	font-size: 1.5rem;
+	text-align: center;
+}
+</style>
